6d4a6ea2e1f929d5542cca124862e261fd129ae1
Eugene Grigoriev <eugene.07@list.ru>
2017-04-01 23:29:40 +0100

    Adds Navigation component
    - react-bootstrap navbar used

---
+++ client/scripts/pages/components/NavBarPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+
+
+const NavigationBar = () => {
+    return (
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="">MS-Basketball</a>
+                </Navbar.Brand>
+            </Navbar.Header>
+        </Navbar>
+    )
+};
+
+export default NavigationBar
--- client/scripts/routes/index.jsx
+++ client/scripts/routes/index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router'
 
 import MasterPage from '../pages/containers/MasterPage'
+import NavigationBarPage from '../pages/components/NavBarPage'
 
 
 const routes = (
     <div>
-        {/*<NavBar />*/}
+        <NavigationBarPage />
         <Switch>
             <Route path="/" component={ MasterPage }/>
             {/*<Route component={NoMatch} />*/}
